3c0cb560ad007a6a411f295280e463d6a329e87b
Dhriti2005 <143262309+Dhriti2005@users.noreply.github.com>
2023-11-08 12:20:11 +0530

    Update fibonacci.java
    This code will ask user the total count of numbers it wants to generate in fibonacci series.Initially, it was printing the last number of fibonacci series. It will now print all the numbers (from 1st number to last number).

--- fibonacci.java
+++ fibonacci.java
@@ -3,29 +3,30 @@ fibonnaci search
 class Main {
 	public static void main(String args[]) {
 		// Your code goes here
-		Scanner scn = new Scanner(System.in);
-		int n = scn.nextInt();
+		 Scanner scn = new Scanner(System.in);
+        System.out.print("Enter how many numbers you want to generate in fibonacci series- ");
+        int n = scn.nextInt();
+        if(n < 0) {
+            System.out.println("N cannot be negative");
+            return;
+        }
+        int a = 0; //0th fibonacci number
+        int b = 1; //1st fibonacci numbe// r
+        System.out.print("The generated fibonacci series is- ");
+        System.out.print(a+"\t"+b+"\t");
 
-		int a = 0; //0th fibonacci number
-		int b = 1; //1st fibonacci number
+        if(n == 0) System.out.println(a);
+        else if(n == 1) System.out.println(b);
 
-		if(n < 0) {
-			System.out.println("N cannot be negative");
-			return;
-		}
-
-		if(n == 0) System.out.println(a);
-		else if(n == 1) System.out.println(b);
 		else {
-			int c = 0;
-			for(int i=2;i<=n;i++) {
-				c = a + b;
-				a = b;
-				b = c;
-			}
+            int c = 0;
+            for(int i=2;i<n;i++) {
+                c = a + b;
+                a = b;
+                b = c;
+                System.out.print(c+"\t");
+            }
+        }
 
-			System.out.println(c);
-		}
-
-	}
+    }
 }
